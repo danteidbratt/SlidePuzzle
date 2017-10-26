18669131e4ef39093e5bdca329a7e18e5c2ea924
Jakob <jakob.lindahl@yh.nackademin.se>
2017-10-26 14:35:14 +0200

    sorry

--- src/slidepuzzle/Bricka.java
+++ src/slidepuzzle/Bricka.java
@@ -1,20 +1,11 @@
 package slidepuzzle;
 
 import javax.swing.*;
-    public class Bricka extends JLabel{
-    String[][] brickor;
+
+public class Bricka extends JLabel {
 
     public Bricka() {
-        int a = 1;
-        brickor = new String[4][4];
-        for (String[] strings : brickor) {
-            for (String string : strings) {
-                string = "" + ++a;
-            }
-        }
-        
-        
+
     }
-        
-        
+
 }
--- src/slidepuzzle/Spelplan.java
+++ src/slidepuzzle/Spelplan.java
@@ -4,10 +4,20 @@ import java.awt.GridLayout;
 import javax.swing.*;
 
 public class Spelplan extends JPanel {
-    
-    Spelplan(int rows, int cols) {
-        setSize(400,200);
-        setLayout(new GridLayout(rows, cols));
-    }
 
+    String[][] bricka;
+
+    Spelplan(int rows, int cols) {
+        setSize(400, 200);
+        setLayout(new GridLayout(rows, cols));
+
+        int a = 1;
+        bricka = new String[4][4];
+        for (String[] strings : bricka) {
+            for (String string : strings) {
+                string = "" + ++a;
+            }
+        }
+
+    }
 }
