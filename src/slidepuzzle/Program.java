438eb984a5f7315c23cb81b1981044af7cc20a45
danteidbratt <danteidbratt@users.noreply.github.com>
2017-10-27 16:19:03 +0200

    PERFECTION

--- src/slidepuzzle/Program.java
+++ src/slidepuzzle/Program.java
@@ -1,12 +1,15 @@
 package slidepuzzle;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
 import javax.swing.*;
 
 public class Program extends JFrame{
+    
+    JButton shuffle = new JButton("Nytt Spel");
 
-    public static Spelplan spelplan = new Spelplan(4,4);
-    public static Meny meny = new Meny();
+    public Spelplan spelplan = new Spelplan(4,4);
+    public Meny meny = new Meny(shuffle);
         
     public void Komponenter(){
         setLayout(new BorderLayout());
@@ -19,5 +22,17 @@ public class Program extends JFrame{
         add(meny, BorderLayout.WEST);
         
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        shuffle.addActionListener((ActionEvent ae) -> {
+            remove(spelplan);
+            spelplan = new Spelplan(meny.getRader(), meny.getKollumner());
+            setSize(meny.getKollumner()*100+200,meny.getRader()*100);
+            setLocationRelativeTo(null);
+            add(spelplan);
+            revalidate();
+            repaint();
+            spelplan.shuffle();
+        });
     }
+    
 }
