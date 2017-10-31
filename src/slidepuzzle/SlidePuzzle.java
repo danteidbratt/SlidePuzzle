76649d02c7c71dd6fa8bf2e35e8fddf4517d3cf1
danteidbratt <danteidbratt@users.noreply.github.com>
2017-10-31 15:31:41 +0100

    blä

--- src/slidepuzzle/SlidePuzzle.java
+++ src/slidepuzzle/SlidePuzzle.java
@@ -5,5 +5,7 @@ public class SlidePuzzle {
     public static void main(String[] args) {
         Program go = new Program();
         go.Komponenter();
+        // Hej på dig.
+        int i = 0;
     }
 }
